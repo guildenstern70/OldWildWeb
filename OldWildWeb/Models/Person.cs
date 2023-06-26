606f4c76337b04faf0f3b306c3270cfd58bc1beb
Alessio Saltarin <alessiosaltarin@gmail.com>
2023-06-26 15:17:02 +0200

    Add project files.

---
+++ OldWildWeb/Models/Person.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace OldWildWeb.Models
+{
+    public class Person
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Surname { get; set; }
+        public int Age { get; set; } = 0;
+        public string eMail { get; set; }
+
+    }
+}
